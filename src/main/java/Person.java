86101853781a5907bfb41b82b7c5966cbea295c8
TaoTaoNing <790250851@qq.com>
2019-07-18 15:43:17 +0800

    '测试分支--》主干的合并'

---
+++ src/main/java/Person.java
@@ -0,0 +1,41 @@
+import com.alibaba.fastjson.annotation.JSONField;
+
+/**
+ * Create by TaoTaoNing
+ * 2019/4/9
+ **/
+public class Person {
+    private Integer id;
+
+    private String shop_id;
+
+    public Person() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getShop_id() {
+        return shop_id;
+    }
+
+    public void setShop_id(String shop_id) {
+        this.shop_id = shop_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+
+                ", shop_id='" + shop_id + '\'' +
+                '}';
+    }
+}
